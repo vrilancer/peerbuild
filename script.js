2e2e1ad335b4be96c8cedc14cdc64546febc92e8
vrilancer <vrilancerofficial@gmail.com>
2026-06-22 22:36:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-```javascript
 const form = document.getElementById("waitlist-form");
 
-form.addEventListener("submit", async (e) => {
-    e.preventDefault();
+if (form) {
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
 
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
 
-    await fetch("https://script.google.com/macros/s/AKfycbzaxhtLn_ymXuAjfZGPQteVKRJrVrBmuglhdX2rj4SW_Vz_BiFG09imxpcrVKVFkKHs/exec", {
-        method: "POST",
-        body: JSON.stringify({
-            name: name,
-            email: email
-        })
-    });
+        await fetch("https://script.google.com/macros/s/AKfycbzaxhtLn_ymXuAjfZGPQteVKRJrVrBmuglhdX2rj4SW_Vz_BiFG09imxpcrVKVFkKHs/exec", {
+            method: "POST",
+            body: JSON.stringify({
+                name: name,
+                email: email
+            })
+        });
 
-    document.getElementById("success-message").innerText =
+        document.getElementById("success-message").innerText =
         "You're on the waitlist.";
 
-    form.reset();
-});
+        form.reset();
+    });
+}
 ```
